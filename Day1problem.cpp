1b9ef2f04f62498c8bd3ce9d09a26084b12eec6b
TheWizardKingg <sharmamohit53998@gmail.com>
2025-09-06 07:39:37 -0400

    Buy_and_sell_stock

--- Day1problem.cpp
+++ Day1problem.cpp
@@ -13,3 +13,6 @@ int main(){
     cout << profit<< endl;
     return 0;
 }
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// we just run a loop until the vector's size and keep updating the maximum between profit and the current price - minimum element so far
+// and also keep updating the minimum element so far with the minimum between the current minimum element and the current price
