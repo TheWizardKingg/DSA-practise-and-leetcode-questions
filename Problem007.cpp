bfa7a3dfbd5208c7fa3f2233502a754b16db770e
TheWizardKingg <sharmamohit53998@gmail.com>
2025-09-08 14:10:28 -0400

    Single_element_in_a_sorted_array_binary_search

---
+++ Problem007.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int singleNonDuplicate(vector<int>& nums) {
+    int start=1, end=nums.size()-2;
+    int mid=start+(end-start)/2;
+    if (nums.size()==1) return nums[0];
+    if (nums[0]!=nums[start]) return nums[0];
+    if (nums[nums.size()-1]!=nums[end]) return nums[nums.size()-1];
+    while (start<=end){
+        if (nums[mid]==nums[mid-1]){
+            if (mid%2==0){
+                end=mid-1;
+            }else{
+                start=mid+1;
+            }
+        }else if(nums[mid]==nums[mid+1]){
+            if (mid%2==0){
+                start=mid+1;
+            }else{
+                end=mid-1;
+            }
+        }else{
+            return nums[mid];
+        }
+        mid=start+(end-start)/2;
+
+    }
+    return -1;
+}
+int main(){
+    vector<int> arr={1,1,2,3,3,4,4,8,8};
+    cout<<singleNonDuplicate(arr);
+    return 0;
+}
