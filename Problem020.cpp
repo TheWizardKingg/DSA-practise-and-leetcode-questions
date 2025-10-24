da35bde445d641acb158470603a6fe23bcc5dc2f
TheWizardKingg <sharmamohit53998@gmail.com>
2025-10-24 13:19:06 +0530

    singly_circular_linked_list

---
+++ Problem020.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+struct node{
+    int data;
+    node* next;
+};
+void insertatbeg(node* end, int val){
+    node* newnode= (struct node*)malloc(sizeof(node));
+    newnode->data=val;
+    newnode->next=end->next;
+    end->next=newnode;
+}
+int main(){
+    node* end=NULL;
+    bool choice=true;
+    int val;
+    while (choice){
+        cout<<"Enter data: ";
+        cin>>val;
+        node* newnode=(struct node*)malloc(sizeof(node));
+        if (val!=-1){
+            newnode->data=val;
+        }else{
+            choice=false;
+            break;
+        }
+        if (end==NULL){
+            end=newnode;
+            end->next=end;
+        }else{
+            newnode->next=end->next;
+            end->next=newnode;
+            end=newnode;
+        }
+    }
+    node* temp=end->next;
+    
+    do{
+        cout<<temp->data;
+        temp=temp->next;
+    }while(temp!=end->next);
+    int VAL;
+    cout<<"Enter any number to insert at beginning";
+    cin>>VAL;
+    insertatbeg(end, VAL);
+    node* Temp=end->next;
+    
+    do{
+        cout<<Temp->data;
+        Temp=Temp->next;
+    }while(Temp!=end->next);
+    return 0;
+}
