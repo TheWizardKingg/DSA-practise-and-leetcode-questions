8bf5a12a7c51e887a71171b9e3f35444cabbbc8d
TheWizardKingg <sharmamohit53998@gmail.com>
2025-10-28 15:41:25 +0530

    Circular_singly_linked_list

--- Problem020.cpp
+++ Problem020.cpp
@@ -4,11 +4,32 @@ struct node{
     int data;
     node* next;
 };
-void insertatbeg(node* end, int val){
+void display(node* end){
+    node* Temp=end->next;
+    
+    
+    do{
+        cout<<Temp->data;
+        Temp=Temp->next;
+    }while(Temp!=end->next);
+}
+void insertatbeg(node* &end, int val){
     node* newnode= (struct node*)malloc(sizeof(node));
     newnode->data=val;
     newnode->next=end->next;
     end->next=newnode;
+}
+void insertatend(node*&end, int val){
+    node* newnode=(struct node*)malloc(sizeof(node));
+    newnode->data=val;
+    newnode->next=end->next;
+    end->next=newnode;
+    end=newnode;
+}
+void deleteatbeg(node* &end){
+    node* TEMP=end->next;
+    end->next=end->next->next;
+    free(TEMP);
 }
 int main(){
     node* end=NULL;
@@ -39,15 +60,17 @@ int main(){
         cout<<temp->data;
         temp=temp->next;
     }while(temp!=end->next);
-    int VAL;
-    cout<<"Enter any number to insert at beginning";
-    cin>>VAL;
-    insertatbeg(end, VAL);
-    node* Temp=end->next;
+    // int VAL;
+    // cout<<"Enter any number to insert at beginning";
+    // cin>>VAL;
+    // insertatbeg(end, VAL);
+    // int a;
+    // cout<<"Enter any number to insert at end";
+    // cin>>a;
+    // insertatend(end, a);
+    deleteatbeg(end);
+    cout<<endl;
+    display(end);
     
-    do{
-        cout<<Temp->data;
-        Temp=Temp->next;
-    }while(Temp!=end->next);
     return 0;
 }
