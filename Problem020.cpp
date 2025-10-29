6c6dc807830eeae461faa2fd3ff9d5ec4220eaca
TheWizardKingg <sharmamohit53998@gmail.com>
2025-10-29 12:31:17 +0530

    singly_circular_linked_list

--- Problem020.cpp
+++ Problem020.cpp
@@ -4,11 +4,57 @@ struct node{
     int data;
     node* next;
 };
-void insertatbeg(node* end, int val){
+void display(node* end){
+    node* Temp=end->next;
+    
+    
+    do{
+        cout<<Temp->data<<" ";
+        Temp=Temp->next;
+    }while(Temp!=end->next);
+}
+void insertatbeg(node* &end, int val){
     node* newnode= (struct node*)malloc(sizeof(node));
     newnode->data=val;
     newnode->next=end->next;
     end->next=newnode;
+}
+void insertatend(node*&end, int val){
+    node* newnode=(struct node*)malloc(sizeof(node));
+    newnode->data=val;
+    newnode->next=end->next;
+    end->next=newnode;
+    end=newnode;
+}
+void deleteatbeg(node* &end){
+    node* TEMP=end->next;
+    end->next=end->next->next;
+    free(TEMP);
+}
+void deleteatend(node* &end){
+    node* TEMP=end->next;
+    node* TEMP2;
+    for (int i=0; TEMP->next!=end; i++){
+        TEMP=TEMP->next;
+    }
+    TEMP2=TEMP;
+    TEMP2=TEMP2->next;
+    TEMP->next=end->next;
+    end=TEMP;
+    free(TEMP2);
+
+}
+void deleteatpos(node* &end, int pos){
+    node * TEMP=end->next;
+    node* TEMP2;
+    for (int i=0; i<pos-2; i++){
+        TEMP=TEMP->next;
+    }
+    TEMP2=TEMP;
+    TEMP=TEMP->next;
+    TEMP2->next=TEMP->next;
+    free(TEMP);
+    
 }
 int main(){
     node* end=NULL;
@@ -33,21 +79,14 @@ int main(){
             end=newnode;
         }
     }
-    node* temp=end->next;
+    int pos;
+    cout<<"Enter the position of the node to delete: ";
+    cin>>pos;
+    deleteatpos(end,pos);
+    cout<<"After deletion at position: ";
+    display(end);
     
-    do{
-        cout<<temp->data;
-        temp=temp->next;
-    }while(temp!=end->next);
-    int VAL;
-    cout<<"Enter any number to insert at beginning";
-    cin>>VAL;
-    insertatbeg(end, VAL);
-    node* Temp=end->next;
-    
-    do{
-        cout<<Temp->data;
-        Temp=Temp->next;
-    }while(Temp!=end->next);
     return 0;
 }
+
+// singly circular linked list implementation complete
