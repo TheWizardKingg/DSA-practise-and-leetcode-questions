40c8e1b5a0ca073e8b52cd0a6709e430cd6fc15c
TheWizardKing <sharmamohit53998@gmail.com>
2025-11-26 23:22:42 +0530

    basic OpenCV image operations

--- Problem038.py
+++ Problem038.py
@@ -10,3 +10,8 @@ c=input("Enter the name to save the image: ")
 cv2.imwrite(c,img)
 name="Mohit"
 print(f"Image saved successfully by {name}.")
+# reading = module.function("<file_name>")                        [file_name is fixed as per the image]
+# showing = module.function("<title>",<reading>)                  [title can be anything as per user choice]
+# writing/saving = module.function("<new_file_name>",<reading>)   [new_file_name can be anything as per user choice] 
+# cv2.COLOR_BGR2GRAY is used to convert the image from BGR to GRAY scale
+# cv2.waitKey(0) is used to keep the image window open until any key
