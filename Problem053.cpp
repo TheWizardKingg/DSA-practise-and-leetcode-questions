c6fcc1bd51b746b5e0b0b7089de512412f686c63
TheWizardKing <sharmamohit53998@gmail.com>
2025-12-12 00:00:06 +0530

    untangling a linked list

---
+++ Problem053.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+struct ListNode{
+    int data;
+    ListNode* next;
+};
+
+ListNode *detectCycle(ListNode *head){
+    ListNode* slow=head;
+    ListNode* fast=head;
+    bool found=false;
+    while(fast!=NULL && fast->next!=NULL){
+        slow=slow->next;
+        fast=fast->next->next;
+        if (fast==slow){
+            found=true;
+            break;
+        }
+    }
+    if (found!=true){
+        return NULL;
+    }
+    
+    slow=head;
+    while(slow!=fast){
+        slow=slow->next;
+        fast=fast->next;
+    }
+    return slow;
+}
+
+int main(){
+    return 0;
+}
