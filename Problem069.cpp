fadc3adb7e4bbc638acf2ad765a02d834e16f0c4
TheWizardKing <sharmamohit53998@gmail.com>
2025-12-13 16:40:19 +0530

    O(n^2) for threeSum

--- Problem069.cpp
+++ Problem069.cpp
@@ -1,36 +1,44 @@
 #include<iostream>
 #include<vector>
+#include<set>
 #include<map>
+#include<algorithm>
 using namespace std;
-vector<vector<int>> threeSum(vector<int> nums){
+vector<vector<int>> threeSum(vector<int>nums){
     vector<vector<int>> ans;
-    vector<int> vec;
     map<int,int> mp;
+    set<vector<int>> st;
+    
     for (int i=0; i<nums.size(); i++){
         mp.insert({nums[i],i});
     }
-
     for (int i=0; i<nums.size(); i++){
         for (int j=i+1; j<nums.size(); j++){
             int target=-(nums[i]+nums[j]);
-            if(mp.find(target)!=mp.end()){
+            if (mp.find(target)!=mp.end()){
                 if(i!=j && j!=mp[target] && mp[target]!=i){
-                    
-                    ans.push_back({nums[i],nums[j],target});
+                    vector<int> vec={nums[i],nums[j],target};
+                    sort(vec.begin(), vec.end());
+                    st.insert(vec);
                 }
             }
         }
+    }
+    for (vector<int> vec: st){
+        ans.push_back(vec);
     }
     return ans;
 }
 int main(){
     vector<int> nums={-1,0,1,2,-1,-4};
-    vector<vector<int>> sol=threeSum(nums);
-    for(vector<int> x: sol){
-        for(int y: x){
+    vector<vector<int>> vec=threeSum(nums);
+    for (vector<int> x: vec){
+        for(auto y:x){
             cout<<y<<" ";
         }
         cout<<endl;
     }
     return 0;
 }
+//dude finally, i got to understand this better approach after coding it for 5 days, i was stuck, kept thinking about how to avoid duplicates
+// and finally i came to know that set also can be used to ensure unique elements inside a vector which blew my mind.
