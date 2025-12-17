884784a51644fdb7ac1b34db25fd47ecf4c62d15
TheWizardKing <sharmamohit53998@gmail.com>
2025-12-17 23:04:25 +0530

    First tree program using recursion

---
+++ Problem059.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+class node{
+public: 
+    int data;
+    node* right;
+    node* left;
+    node(int val){
+        data=val;
+        right=NULL;
+        left=NULL;
+    }
+    void BuildTree(int PreArr[]){
+        
+    }
+};
+
