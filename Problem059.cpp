c1fc244c090459730814b026221790ebae1e9b8a
TheWizardKing <sharmamohit53998@gmail.com>
2025-12-18 23:54:49 +0530

    Tree program (recursion)

--- Problem059.cpp
+++ Problem059.cpp
@@ -1,21 +1,54 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-class node{
-public: 
+
+struct Node {
     int data;
-    node* right;
-    node* left;
-    node(int val){
-        data=val;
-        right=NULL;
-        left=NULL;
-    }
-    node* BuildTree(int PreArr[], int size, int index){
-        if(PreArr[index]==-1 || index>=size){
-            return NULL;
-        }
-        node* root=new node(PreArr[index]);
-        
+    Node* left;
+    Node* right;
+
+    Node(int val) {
+        data = val;
+        left = NULL;
+        right = NULL;
     }
 };
 
+Node* buildTree() {
+    int data;
+    cout << "Enter data (-1 for no node): ";
+    cin >> data;
+
+    if (data == -1)
+        return nullptr;
+
+    Node* root = new Node(data);
+
+    cout << "Enter left child of " << data << endl;
+    root->left = buildTree();
+
+    cout << "Enter right child of " << data << endl;
+    root->right = buildTree();
+
+    return root;
+}
+
+// Just to verify your tree isn’t imaginary
+void inorder(Node* root) {
+    if (root == nullptr) return;
+
+    inorder(root->left);
+    cout << root->data << " ";
+    inorder(root->right);
+}
+
+int main() {
+    Node* root = nullptr;
+
+    root = buildTree();
+
+    cout << "Inorder traversal: ";
+    inorder(root);
+    cout << endl;
+
+    return 0;
+}
