ced05aba92e8b8ec2815b657747d1e6d4e9f677e
Mohit sharma <sharmamohit53998@gmail.com>
2026-02-27 15:54:16 +0530

    Palindrome or not

---
+++ BasicMath/BM-3.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+bool PalindromeOrNot(int number){
+    int num=number;
+    int rev=0;
+    while (num>0){
+        int rem=num%10;
+        rev=rev*10+rem;
+        num=num/10;
+    }
+    if(number==rev){
+        return true;
+    }
+    return false;
+}
+int main(){
+    int num;
+    cout<<"Enter the number: ";
+    cin>>num;
+    if(PalindromeOrNot(num)){
+        cout<<"The number is a palindrome.";
+    }
+    else{
+        cout<<"The number is not a palindrome.";
+    }
+    return 0;
+}
