8b107edcdca85f596d69a1aab1aa06b143925f47
Mohit sharma <sharmamohit53998@gmail.com>
2026-02-27 14:27:30 +0530

    Last updated 27-feb-2026

--- Problem039.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-dict1={'Name':['Tom','nick','krish','jack'],
-        'Age':[20,21,19,18],
-        'Marks':[85,86,84,88]}
-a=pd.DataFrame(dict1)
-b=a.loc["Age"[0]]
-print(b)
-# print(a)
