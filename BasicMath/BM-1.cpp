f399722ad60bc6a45f2e8619651b8fa2a89ef764
Mohit sharma <sharmamohit53998@gmail.com>
2026-02-27 15:29:32 +0530

    count digits in a number using log

--- BasicMath/BM-1.cpp
+++ BasicMath/BM-1.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<cmath>
 using namespace std;
 int countDigitsOfNumber(int num){
     int n=num;
@@ -9,12 +10,18 @@ int countDigitsOfNumber(int num){
     return i-1;
 
 }
+int countDigitsOfNumberUsingLog(int num){
+    return (int)log10(num)+1;
+}
 int main(){
     int n;
     cout<<"Enter a number: ";
     cin>>n;
     cout<<"There are "<<countDigitsOfNumber(n)<<" digits in the number"<<endl;
+    cout<<"There are "<<countDigitsOfNumberUsingLog(n)<<" digits in the number using log method"<<endl;
     return 0;
 }
 
 //we just keep dividing by 10, until it becomes zero, once it is zero, just return the "i-1" 
+//we can also use log10 to find the number of digits, because log10 of a number gives us the power to which 
+// 10 must be raised to get that number, so if we add 1 to that, we get the number of digits in that number.
