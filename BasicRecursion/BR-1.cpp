032f1bdcc081f35425cedf6d675862639c9da603
Mohit sharma <sharmamohit53998@gmail.com>
2026-02-28 14:41:25 +0530

    printing/sum of n natural numbers

--- BasicRecursion/BR-1.cpp
+++ BasicRecursion/BR-1.cpp
@@ -1,6 +1,10 @@
 #include<iostream>
 using namespace std;
 int n=0;
+int i=0;
+
+//this function is to print something n times using recursion
+
 void Print_N_Times(){
     if (n==10){
         return;
@@ -9,7 +13,25 @@ void Print_N_Times(){
     n++;
     Print_N_Times();
 }
+
+//below function is to print sum of first 10 natural numbers using recursion
+
+void Sum_N_numbers(int sum){
+    if(i==10){
+        cout<<"Sum of first 10 natural numbers is: "<<sum<<endl;
+        return ;
+    }
+    sum+=i+1;
+    i++;
+    Sum_N_numbers(sum);
+}
+
 int main(){
     cout<<"-----Printing Ten Times-----"<<endl;
     Print_N_Times();
+    cout<<"-----Sum of first 10 natural numbers-----"<<endl;
+    Sum_N_numbers(0);
 }
+
+//just make sure u have a base case to stop avoid infinite calls, and then make sure to update the parameters in the recursive calls
+
