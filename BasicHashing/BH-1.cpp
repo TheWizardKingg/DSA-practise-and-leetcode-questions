a9668fecec1012ec46e0c89d678d60adc52e1e2d
Mohit sharma <sharmamohit53998@gmail.com>
2026-03-04 18:16:46 +0530

    counting frequencies of the array elements

--- BasicHashing/BH-1.cpp
+++ BasicHashing/BH-1.cpp
@@ -1,24 +1,26 @@
-#include <iostream>
+#include<iostream>
+#include<unordered_map>
+
 using namespace std;
-
-int main() {
-    int arr[] = {1, 3, 2, 1, 4, 2, 1};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    int maxVal = 100;   // set according to constraints
-    int hash[101] = {0};  // initialize all to 0
-
-    // Counting frequencies
-    for (int i = 0; i < n; i++) {
-        hash[arr[i]]++;
-    }
-
-    // Printing frequencies
-    for (int i = 0; i <= maxVal; i++) {
-        if (hash[i] > 0) {
-            cout << i << " -> " << hash[i] << endl;
+int main(){
+    unordered_map<int, int> map;
+    int arr[5]={1, 2, 1, 3, 2};
+    int query[5]={1, 3, 4, 2, 10};
+    int count=0;
+    for (int j=0; j<5; j++){
+        count=0;
+        for(int i=0; i<5; i++){
+            if(query[j]==arr[i]) count++;
         }
+        map.insert({query[j],count});
     }
-
+    cout<<"Frequencies are: "<<endl;
+    for(auto it: map){
+        cout<<it.first<<" "<<it.second<<endl;
+    } 
     return 0;
+    
 }
+
+//just include the map, run a for loop, store each element and its occurence in the map
+//that's it
