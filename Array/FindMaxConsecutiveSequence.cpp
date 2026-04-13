4d3d3442a57376f52845cbd4489af6efcdacfab9
TheWizardKing <sharmamohit53998@gmail.com>
2026-04-13 23:06:45 +0530

    Finding the size of maximum consecutive sequence

--- Array/FindMaxConsecutiveSequence.cpp
+++ Array/FindMaxConsecutiveSequence.cpp
@@ -1,31 +1,37 @@
 #include<iostream>
-#include<map>
 #include<vector>
+#include<unordered_set>
 #include<algorithm>
 #include<climits>
 using namespace std;
 
-int findMaxConsecutiveSequence(vector<int>& nums){
-    map<int, int> map;
-    int streakcount=1;
-    int maxStreak=INT_MIN;
-    for(int i=0; i< nums.size(); i++){
-        map.insert({nums[i], i});
+int FindMaxConsecutiveSequence(vector<int>& nums){
+    unordered_set<int> set;           //we use set datastructure because it stores unique values and we can search for O(1) time complexity
+    int count; 
+    int maxCount=INT_MIN;
+    int tempElement;
+
+    if(nums.size() == 0) return 0;        //return if the array is empty (edge case)
+
+    for(int i=0; i<nums.size(); i++){      //store all the element of the array into the set
+        set.insert(nums[i]);
     }
-    int pivot=nums[0];
-    for(int i=0; i<nums.size(); i++){
-        if(map.find(pivot+1)!=map.end()){
-            streakcount++;
-            maxStreak=max(maxStreak, streakcount);
-        }else{
-            streakcount=1;
+
+    for(auto it: set){              //iterate over set elements 
+        if(set.find(it-1)== set.end()){        //check if the previous element of the current element is present or not
+            count=1;                           //if not then we know that the current element is the starting point of the sequence
+            tempElement=it;
+            while(set.find(++tempElement)!=set.end()){      //keep increasing count until we keep finding the next consecutive elements
+                count++;
+            }
+            maxCount=max(maxCount, count);       //eventually update maxCount with maximum count of the recent consecutive sequence
         }
     }
-    return maxStreak;
+    return maxCount;     //ultimately return the maximum count of the biggest consecutive sequence 
 }
 
 int main(){
     vector<int> nums={100, 4, 200, 1, 3, 2};
-    cout<<findMaxConsecutiveSequence(nums);
+    cout<<FindMaxConsecutiveSequence(nums);
+    return 0;
 }
-        
