31391b8dcc5ba790a23ddfbe416d7a4a87ee5316
TheWizardKing <sharmamohit53998@gmail.com>
2026-06-08 23:05:47 +0530

    Finding majority elements which appears more than n/3 times

--- Array/MajorityElement2.cpp
+++ Array/MajorityElement2.cpp
@@ -6,30 +6,30 @@ class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
         vector<int> ans;
-        int count1=0, count2=0, el1=INT_MIN, el2=INT_MIN;
+        int count1=0, count2=0, el1=INT_MIN, el2=INT_MIN;   //atmost 2 majority elements can be there, wo we maintain 2 counters and 2 elements
         for(int i=0; i<nums.size(); i++){
             if(nums[i]==el1){
-                count1++;
+                count1++;        //might not be the actual count of majority element, more like a score which is increased when we encounter same elements
             }else if(nums[i]==el2){
-                count2++;
+                count2++;        //might not be the actual count of majority element, more like a score which is increased when we encounter same elements
             }else if(count1==0 && nums[i]!=el2){
                 el1=nums[i];
                 count1=1;
             }else if(count2==0 && nums[i]!=el1){
                 el2=nums[i];
                 count2=1;
-            }else{
+            }else{        //since the element is unique, it will decrease the count of both majority elements
                 count1--;
                 count2--;
             }
         }
         count1=0;
         count2=0;
-        for(int val: nums){
+        for(int val: nums){        //we now find actual counts of the potential majority elements
             if(val==el1) count1++;
             else if(val==el2) count2++;
         }
-        if(count1>(int)nums.size()/3) ans.push_back(el1);
+        if(count1>(int)nums.size()/3) ans.push_back(el1);      //if they occur more than n/3 times, they get added
         if(count2>(int)nums.size()/3) ans.push_back(el2);
         return ans;
         
