930688c1a8fb28729bad4646881219ecb51aae94
TheWizardKing <sharmamohit53998@gmail.com>
2026-08-02 11:39:40 +0530

    Find the minimum time required to paint the biggest board by painter's partitioning

--- BinarySearch/PaintersPartitionProblem.cpp
+++ BinarySearch/PaintersPartitionProblem.cpp
@@ -8,40 +8,40 @@ bool isValid(int limit, int A, int B, vector<int>& nums){
     int totalTime=0;
     for(int i=0; i<nums.size(); i++){
 
-        if(totalTime + (nums[i]*B) <= limit){
+        if(totalTime + (nums[i]*B) <= limit){    //if total time is lesser/equal than the current chosen limit, then add it to the current painter's total timeual
             totalTime += nums[i]*B;
-        }else{
+        }else{     //if current total time exceeds the current chosen limit, then assign a new painter, and reset the totalTime to current board's time
             actualPainters++;
             totalTime = nums[i]*B;
         }
     }
-    return actualPainters <= A;
+    return actualPainters <= A;    //if actual painters required is lesser/equal than given painters, then return true, otherwise false
 }
 
 int paint(int A, int B, vector<int>& nums) {
     int maxEle= INT_MIN;
     int sum=0;
 
-    for(int x: nums){
+    for(int x: nums){    //calculated sum, maximum value simultaneously, and multiplying by time according to question
         sum += x*B;
         maxEle=max(maxEle, x);
     }
     
-    int start=maxEle*B;
-    int end=sum;
+    int start=maxEle*B;   //maximum painters given can be equal/more than size of array, hence one painter paints one board, hence maximum time taken by any painter will be maximum board length * time units
+    int end=sum;   //minimum painters given can be 1, hence he'll have to paint all the boards, and hence total time would be sum*B (calculated above)
     int answer=-1;
 
     while(start <= end){
         int mid = start + (end-start) / 2;
 
-        if(isValid(mid, A, B, nums)){
+        if(isValid(mid, A, B, nums)){   //if current value is valid, then save it and try finding an even smaller value (if any)
             answer=mid;
             end=mid-1;
-        }else{
+        }else{      //if current value is not valid, then try finding a bigger value instead
             start=mid+1;
         }
     }
-    return answer;
+    return answer%10000003;     //return the valid answer so far (answer%10000003 is done to avoid integer overflow, as the answer can be very large)
 
 }
 
