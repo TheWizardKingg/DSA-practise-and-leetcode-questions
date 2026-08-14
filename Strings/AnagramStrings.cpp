8a4ce04b01bb6a58c4e52d64cd342f2f83e109db
TheWizardKing <sharmamohit53998@gmail.com>
2026-08-14 22:55:14 +0530

    Check whether the two given strings are Anagram or not

--- Strings/AnagramStrings.cpp
+++ Strings/AnagramStrings.cpp
@@ -1,31 +1,30 @@
 #include<iostream>
-#include<strings>
+#include<string>
 using namespace std;
 
 bool isAnagram(string s, string t) {
-    if(s.size() != t.size()) return false;
+    if(s.size() != t.size()) return false;   //if size mismatch, then it can't be an anagram
 
-    int freq1[26] = {0};
-    int freq2[26] = {0};
+    int freq1[26] = {0};     //make two fixed arrays of size 26 (this one for 's' letter's frequencies)
+    int freq2[26] = {0};     //(this one for 't' letter's frequencies)
 
     for (int i=0; i<s.size(); i++){
-        freq1[s[i] - 'a']++;
-        freq2[t[i] - 'a']++;
+        freq1[s[i] - 'a']++;     //calculate the index of the frequency array by subtracting ASCII codes for 's'
+        freq2[t[i] - 'a']++;     //do the same for 't'
     }
 
     for (int j=0; j<26; j++){
-        if(freq1[j] != freq2[j]) return false;
+        if(freq1[j] != freq2[j]) return false;     //just check all the frequencies, at any point of mismatch, return false
     }
-    return true;
+    return true;    //all loops ran successfully, return true
 }
 
 int main() {
-    Solution obj;
 
     string s = "anagram";
     string t = "nagaram";
 
-    cout << obj.isAnagram(s, t);
+    cout << isAnagram(s, t);
 
     return 0;
 }
