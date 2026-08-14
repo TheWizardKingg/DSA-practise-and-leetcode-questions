785f6bc709d179b55d20f26ba7eb98193e2cfaf0
TheWizardKing <sharmamohit53998@gmail.com>
2026-08-14 21:52:48 +0530

    Check if the two strings are rotated or not

--- Strings/RotatedStrings.cpp
+++ Strings/RotatedStrings.cpp
@@ -3,17 +3,17 @@
 using namespace std;
 
 bool rotateString(string s, string goal) {
-    if(s.size() != goal.size()) return false;
+    if(s.size() != goal.size()) return false;      //if the size is not equal, then it can't ever be its rotation
 
     int k = goal.size();
 
-    for(int i=0; i<k; i++){
+    for(int i=0; i<k; i++){     //basically search goal string till we find the first letter of s in it
         if(s[0] == goal[i]){
             int idx = i;
             int j = 0;
             int iterations = 0;
 
-            while(s[j] == goal[idx % k]){
+            while(s[j] == goal[idx % k]){   //after finding the first char, we then check for the whole string, if it matches, we return true
                 j++;
                 idx++;
                 iterations++;
@@ -22,7 +22,7 @@ bool rotateString(string s, string goal) {
 
         }
     }
-    return false;
+    return false;   //return false if all the loops ran but didn't find any permutation of s in goal
 }
 
 int main(){
