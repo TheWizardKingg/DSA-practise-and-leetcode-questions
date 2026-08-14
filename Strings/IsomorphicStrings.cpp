9d1b0b3d2a3499bf798484be489b17be4488a01b
TheWizardKing <sharmamohit53998@gmail.com>
2026-08-14 22:10:00 +0530

    Check whether two strings are isomorphic or not

--- Strings/IsomorphicStrings.cpp
+++ Strings/IsomorphicStrings.cpp
@@ -8,13 +8,30 @@ bool isIsomorphic(string s, string t) {
     if(s.size() != t.size()) return false;
     map<char, char> mp;
     map<char, char> mp2;
+
     for(int i=0; i<s.size(); i++){
-        if(mp.find(s[i]) != mp.end()){
-            if(mp[s[i]] != t[i] ) return false;
+        if(mp.find(s[i]) != mp.end()){  //make one hashmap to store s->t mapping
+            if(mp[s[i]] != t[i]){
+                return false;
+            }
         }else{
             mp.insert({s[i], t[i]});
+        }
+
+        if(mp2.find(t[i]) != mp2.end()){   //make another hashmap to store t->s mapping
+            if(mp2[t[i]] != s[i]){
+                return false;
+            }
+        }else{
             mp2.insert({t[i], s[i]});
         }
+
+        if(mp.find(s[i]) == mp.end() && mp2.find(t[i]) == mp2.end()){    //in case if both mapping don't exist primarily
+            mp.insert({s[i], t[i]});    //insert both simultaneously
+            mp2.insert({t[i], s[i]}); 
+        }
+
+
     }
     return true;
 }
