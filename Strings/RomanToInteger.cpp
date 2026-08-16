4209c6dbc53d7b90aa281ba681335ee6a23c3908
TheWizardKing <sharmamohit53998@gmail.com>
2026-08-16 22:01:28 +0530

    Convert Roman numbers into integers Recursive approach

--- Strings/RomanToInteger.cpp
+++ Strings/RomanToInteger.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <map>
+#include <climits>
 #include <string>
 using namespace std;
 
@@ -35,3 +36,43 @@ int main() {
 
     return 0;
 }
+
+//this is my own recursive approach
+
+// int recursive_func(string s, map<char, int>& mp){
+//     if(s == "") return 0;
+//     if(s.size() == 1) return mp[s[0]];
+
+//     int tempSum = 0;
+//     int maxValue = INT_MIN;
+//     char maxChar;
+//     int index;
+
+//     for(int i=0; i<s.size(); i++){
+//         if(mp[s[i]] > maxValue){
+//             index = i;
+//             maxChar = s[i];
+//             maxValue = mp[s[i]];
+//         }
+//     }
+//     string left = s.substr(0, index);
+//     string right = s.substr(index+1, s.size()-index-1);
+
+//     return (int)(maxValue - recursive_func(left, mp)) + (int)recursive_func(right, mp);
+// }
+
+// int romanToInt(string s) {
+//     map<char,int> mp;
+
+//     mp.insert({'I', 1});
+//     mp.insert({'V', 5});
+//     mp.insert({'X', 10});
+//     mp.insert({'L', 50});
+//     mp.insert({'C', 100});
+//     mp.insert({'D', 500});
+//     mp.insert({'M', 1000});
+    
+//     return recursive_func(s, mp);
+
+    
+// }
