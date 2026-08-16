c2f62df385f4d2112c2795a6973fb4849ddc681b
TheWizardKing <sharmamohit53998@gmail.com>
2026-08-16 10:40:39 +0530

    Sort characters in a string based on their frequencies

--- Strings/SortCharactersByFreq.cpp
+++ Strings/SortCharactersByFreq.cpp
@@ -6,26 +6,26 @@
 using namespace std;
 
 string frequencySort(string s) {
-    unordered_map<char,int> mp;
+    unordered_map<char,int> mp;   
     vector<pair<char,int>> vec;
     string answer="";
 
-    for(int i=0; i<s.size(); i++){
+    for(int i=0; i<s.size(); i++){       //store {char, frequency} in map
         if(mp.find(s[i]) != mp.end()){
             mp[s[i]]++;
         }else{
             mp.insert({s[i], 1});
         }
     }
-    for(auto x: mp){
+    for(auto x: mp){    //store all the map contents into a vector to sort them based on their values (map only sorts based on keys )
         vec.push_back(x);
     }
-    sort(vec.begin(), vec.end(), [](auto &a, auto &b){return a.second > b.second;});
+    sort(vec.begin(), vec.end(), [](auto &a, auto &b){return a.second > b.second;});    //write a sort function which sorts based on second values (aka frequencies)
 
-    for(auto x: vec){
+    for(auto x: vec){      //make a new string and add the characters times their frequency in the string
         answer += string(x.second, x.first);
     }
-    return answer;
+    return answer;    //return the final answer
 
 
 }
