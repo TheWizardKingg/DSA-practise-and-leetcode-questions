3d6999579403f2ac3059cea9e031d9ed60bde989
TheWizardKing <sharmamohit53998@gmail.com>
2026-08-18 00:36:32 +0530

    Convert String to integer

--- Strings/StringToInteger.cpp
+++ Strings/StringToInteger.cpp
@@ -8,35 +8,34 @@ int myAtoi(string s) {
     long long sum = 0;
     bool isNeg = false;
     int i = 0;
-
-    while(i < s.size() && s[i] == ' ') {
+      
+    while(i < s.size() && s[i] == ' ') {     //skip the leading spaces 
         i++;
     }
 
-    if(i < s.size() && s[i] == '-') {
+    if(i < s.size() && s[i] == '-') {    //check whether - or + occurs after skipping spaces or any 0z
         isNeg = true;
         i++;
-    }
-    else if(i < s.size() && s[i] == '+') {
+    }else if(i < s.size() && s[i] == '+') {   
         i++;
     }
 
-    while(i < s.size() && s[i] >= '0' && s[i] <= '9') {
+    while(i < s.size() && s[i] >= '0' && s[i] <= '9') {     //now start adding the string as integer into the sum and check for overflow every time
         sum = sum * 10 + (s[i] - '0');
 
-        if(!isNeg && sum > INT_MAX)
+        if(!isNeg && sum > INT_MAX)   //round off to 2^31 - 1
             return INT_MAX;
 
-        if(isNeg && -sum < INT_MIN)
+        if(isNeg && -sum < INT_MIN)   //round off to -2^31
             return INT_MIN;
 
         i++;
     }
 
     if(isNeg)
-        sum = -sum;
+        sum = -sum;    //return negative number if string is negative 
 
-    return (int)sum;
+    return (int)sum;     //return answer as integer as per question's primary requirement
 }
 
 int main(){
