b2bd84613e73681a811f144a61cc08c8cc1c4c4e
TheWizardKing <sharmamohit53998@gmail.com>
2026-08-22 20:54:06 +0530

    First Linked List program

--- LinkedList/SingleLinkedList.cpp
+++ LinkedList/SingleLinkedList.cpp
@@ -7,7 +7,7 @@ public:
     int data;
     Node* next;
 
-    Node(int val){
+    Node(int val){     //make a class Node with value, and a pointer to next node
         data = val;
         next = NULL;
     }
@@ -20,11 +20,11 @@ class List{
 public:
 
     List(){
-        head = tail = NULL;
+        head = tail = NULL;   //initialize the pointers with NULL
     }
 
-    void push_front(int val){
-        Node* newnode = new Node(val);
+    void push_front(int val){       //make a node, if its first node then (head=tail=newnode) otherwise update forward mapping, then update previous mapping
+        Node* newnode = new Node(val);    //forward mapping first: (newnode->next = head), backward mapping second: (head = newnode)
 
         if(head == NULL){
             head = tail = newnode;
@@ -34,7 +34,7 @@ public:
         }
     }
 
-    void pop_front(){
+    void pop_front(){     //if list isn't empty, make temp pointer to head, update the head, delete the temp
         if(head != NULL){
             Node* temp = head;
             head = head->next;
@@ -47,25 +47,25 @@ public:
 
     void Push_at_position(int val, int position){
         Node* temp = head;
-        for(int i=1; i<position-1; i++){
-            temp = temp->next;
-        }
+        for(int i=1; i<position-1; i++){    //this stops the temp pointer to 1 node before the actual deletion node 
+            temp = temp->next;          //example: we want to delete 5 (position=4) from [4,6,3,5,7,8,9], this loop will update temp and stop temp at 3
+        }                              //total 2 iterations for this loop to update temp to 3 (position=3)
 
-        Node* newnode = new Node(val);
+        Node* newnode = new Node(val);       //make a new node to insert
 
-        newnode->next = temp->next;
-        temp->next = newnode;
+        newnode->next = temp->next;     //update forward mapping first (newnode->next = temp->next)
+        temp->next = newnode;          //update backward mapping later (temp->next = newnode)
     }
 
     void Pop_at_position(int position){
-        Node* temp = head;
+        Node* temp = head;           // make two temp pointers (since one is going to be sacrificed for deletion of the node)
         Node* temp2;
 
-        for(int i=1; i<position-1; i++){
+        for(int i=1; i<position-1; i++){      //again skip temp to one node before the actual desired node 
             temp = temp->next;
         }
-        temp2 = temp->next;
-        temp->next = temp->next->next;
+        temp2 = temp->next;        //update forward mapping
+        temp->next = temp->next->next;   //update backward mapping
         delete(temp2);
     }
 
