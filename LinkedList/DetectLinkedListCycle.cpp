098ea3a9eb7a8504ce8b336f3b193e5940e060e1
TheWizardKing <sharmamohit53998@gmail.com>
2026-08-24 22:55:46 +0530

    Detect whether the linked list contains loops or not

--- LinkedList/DetectLinkedListCycle.cpp
+++ LinkedList/DetectLinkedListCycle.cpp
@@ -16,21 +16,21 @@ public:
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-        if(head == NULL || head->next == NULL) return false;
+        if(head == NULL || head->next == NULL) return false;    //base case for lists containing only one or two elements (can't have loops)
 
-        ListNode* slowPtr = head;
-        ListNode* fastPtr = head;
+        ListNode* slowPtr = head;   //slow pointer
+        ListNode* fastPtr = head;   //fast pointer
 
-        while(fastPtr != NULL && fastPtr->next != NULL){
+        while(fastPtr != NULL && fastPtr->next != NULL){    //unless the fastpointer becomes NULL or reaches the end node, run the loop
 
-            slowPtr = slowPtr->next;
-            fastPtr = fastPtr->next->next;
+            slowPtr = slowPtr->next;      //increment slow pointer by one node
+            fastPtr = fastPtr->next->next;    //fast pointer moves at twice as fast as slow pointer
             
-            if(slowPtr == fastPtr) return true;
+            if(slowPtr == fastPtr) return true;       //if loop exists then at some point of time, both of the pointers are sure to coincide (aka true)
         }
             
 
-        return false;
+        return false;    //return false if fast pointer has reached the end and still hasn't coincided with the slow pointer yet
     }
 };
 
