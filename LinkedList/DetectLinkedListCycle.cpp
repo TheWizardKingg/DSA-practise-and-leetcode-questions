cdbeac7e8456f8467de84d22205e10780661a3e9
TheWizardKing <sharmamohit53998@gmail.com>
2026-08-24 22:53:04 +0530

    Detect whether the linked list has loops or not

---
+++ LinkedList/DetectLinkedListCycle.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+using namespace std;
+
+class ListNode{
+public:
+    int data;
+    ListNode* next;
+
+    ListNode(int x){
+        data = x;
+        next = NULL;
+
+    }
+};
+
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        if(head == NULL || head->next == NULL) return false;
+
+        ListNode* slowPtr = head;
+        ListNode* fastPtr = head;
+
+        while(fastPtr != NULL && fastPtr->next != NULL){
+
+            slowPtr = slowPtr->next;
+            fastPtr = fastPtr->next->next;
+            
+            if(slowPtr == fastPtr) return true;
+        }
+            
+
+        return false;
+    }
+};
+
