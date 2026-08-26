452a6209d1a3381f40b63b1dbedd3296109b0530
TheWizardKing <sharmamohit53998@gmail.com>
2026-08-26 16:21:57 +0530

    Find the length of the loop in linked list

---
+++ LinkedList/LengthOfLoopInList.cpp
@@ -0,0 +1,48 @@
+#include<iostream>
+using namespace std;
+
+class ListNode{
+public:
+    int data;
+    ListNode* next;
+
+    ListNode(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+class Solution {
+public:
+    int detectCycle(ListNode *head) {
+        ListNode* slowPtr = head;
+        ListNode* fastPtr = head;
+
+        while(fastPtr != NULL && fastPtr->next != NULL){  //if fastPtr ever reaches NULL, the list doesn't contain any loop
+            slowPtr = slowPtr->next;
+            fastPtr = fastPtr->next->next;
+            if(slowPtr == fastPtr) break;   //if pointers collide, hence, we reset the pointers and update by one now
+
+        }
+        if(fastPtr == NULL || fastPtr->next == NULL) return NULL;    
+
+        fastPtr = head;   //reset pointer to head
+
+        while(slowPtr != fastPtr){   //update by one now, until both collide
+            slowPtr = slowPtr->next;
+            fastPtr = fastPtr->next;
+
+        }
+
+        ListNode* temp = slowPtr;
+        int count = 0;
+
+        while(temp != slowPtr){
+            count++;
+            temp= temp->next;
+        }
+
+        return count;
+    }
+};
+
