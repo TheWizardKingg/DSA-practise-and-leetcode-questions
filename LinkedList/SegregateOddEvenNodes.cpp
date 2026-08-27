e0ad20ada5a9f698a9b8508de7048577c6b6c87d
TheWizardKing <sharmamohit53998@gmail.com>
2026-08-27 23:53:15 +0530

    Segregate all the even and odd nodes in a linked list

--- LinkedList/SegregateOddEvenNodes.cpp
+++ LinkedList/SegregateOddEvenNodes.cpp
@@ -14,24 +14,24 @@ public:
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head == NULL || head->next == NULL) return head;
+        if(head == NULL || head->next == NULL) return head;   //edge case where list contains zero or only one element 
         ListNode* oddPtr = head;
         ListNode* evenPtr = head->next;
+        ListNode* lastOdd = oddPtr;
         ListNode* firstEven = evenPtr;
-        ListNode* lastOdd;
 
         while(true){
             if(oddPtr->next == NULL || evenPtr->next == NULL) break;
-            oddPtr->next = oddPtr->next->next;
+            oddPtr->next = oddPtr->next->next;   //keep mapping all the odd nodes together
             oddPtr = oddPtr->next;
-            lastOdd = oddPtr;
+            lastOdd = oddPtr;    //keep track of the last odd node in order to connect it with the first even node 
 
-            evenPtr->next = evenPtr->next->next;
-            evenPtr = evenPtr->next;
+            evenPtr->next = evenPtr->next->next;       //keep mapping all the even nodes together
+            evenPtr = evenPtr->next;    //update the even pointer as well
         }
 
-        lastOdd->next = firstEven;
+        lastOdd->next = firstEven;    // at last, connect the lastOdd mapping to first Even node, hence all the linked list has been separated 
 
-        return head;
+        return head;   //return head finally
     }
 };
