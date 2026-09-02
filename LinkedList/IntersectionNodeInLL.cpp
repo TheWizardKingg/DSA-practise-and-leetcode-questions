9c34b198b77c913f73cf86c9e23fcbaa90dbfdde
TheWizardKing <sharmamohit53998@gmail.com>
2026-09-02 23:24:51 +0530

    Find the intersection Node in the linked list

--- LinkedList/IntersectionNodeInLL.cpp
+++ LinkedList/IntersectionNodeInLL.cpp
@@ -15,10 +15,10 @@ public:
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp1 = headA;
-        ListNode* temp2 = headB;
-
-        while(temp1 != temp2){
+        ListNode* temp1 = headA;    //length of list A is unknown and length of list B is unknown as well as A.length != B.length
+        ListNode* temp2 = headB;   //but A+B == B+A, so we make two pointers and traverse them on both lists,
+                                   //as soon as they hit NULL in their respective lists, we put them onto the other list, 
+        while(temp1 != temp2){        //so they would eventually meet at the intersection point or at NULL if there is no intersection at all.
             if(temp1 == NULL) temp1 = headB;
             else temp1 = temp1->next;
 
