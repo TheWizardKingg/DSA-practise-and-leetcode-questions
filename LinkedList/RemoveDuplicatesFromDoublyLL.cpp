f7fa42ae8fb33ed4f24dab2d5ff9168f0a5661c2
TheWizardKing <sharmamohit53998@gmail.com>
2026-09-04 16:01:35 +0530

    Remove all the duplicates from the doubly linked list

--- LinkedList/RemoveDuplicatesFromDoublyLL.cpp
+++ LinkedList/RemoveDuplicatesFromDoublyLL.cpp
@@ -18,11 +18,11 @@ public:
 class Solution {
 public:
     ListNode * removeDuplicates(ListNode *head) {
-        unordered_set<int> st;
-        ListNode* temp = head;
+        unordered_set<int> st;  //using an unordered set to store all the unique values of the linked list nodes
+        ListNode* temp = head;   //unordered set uses hashing, hence O(1) time comp for insertion and searching unlike normal set which uses BST, O(logn)
 
         while(temp != NULL){
-            if (st.find(temp->val) != st.end()){
+            if (st.find(temp->val) != st.end()){   //if value exists already, then just delete that particular node and update pointers accordingly
 
                 ListNode* back = temp->prev;
                 back->next = temp->next;
@@ -35,11 +35,11 @@ public:
                 temp = temp->next;
                 delete(tempDelNode);
 
-            }else{
+            }else{    //if value doesn't exist, then insert it into the unordered set and move forward
              st.insert(temp->val);
                 temp = temp->next;
             }
         }
-        return head;
+        return head;    //return head eventually
     }
 };
