737b7869f2371064e6f600dac54bebf96266f32e
TheWizardKing <sharmamohit53998@gmail.com>
2026-09-04 23:58:59 +0530

    Rotate the given linked list by k nodes to the right

--- LinkedList/RotateTheLinkedListByK.cpp
+++ LinkedList/RotateTheLinkedListByK.cpp
@@ -15,27 +15,27 @@ public:
 class Solution {
 public:
     ListNode* rotateRight(ListNode* head, int k) {
-        if(head == NULL) return NULL;
+        if(head == NULL) return NULL;    //if linked list is empty, no point rotating
         ListNode* temp = head;
         ListNode* originalHead = head;
-        int size = 0;
-        while(temp != NULL){
+        int size = 0;    
+        while(temp != NULL){      //find the size of the linked list
             size++;
             temp = temp->next;
         }
-        k = k%size;
-        if(k == 0) return originalHead;
+        k = k%size;    //check how much rotation do we actually need to do (cause here, k is allowed to be greater than the size)
+        if(k == 0) return originalHead;   //if actual k after mod comes out to be 0, then no point in rotating the list
         temp = head;
-        for(int i=1; i<size-k; i++){
+        for(int i=1; i<size-k; i++){     //skip to the node just before the new head after rotation
             temp = temp->next;
         }
-        head = temp->next;
+        head = temp->next;    //update the pointer mappings
         temp->next = NULL;
         temp = head;
-        while(temp->next != NULL){
+        while(temp->next != NULL){     //skip to the end of the rotated linked list
             temp = temp->next;
         }
-        temp->next = originalHead;
-        return head;
+        temp->next = originalHead;   //connect it to the original head of the linked list
+        return head;     //return head of the rotated linked list
     }
 };
