aa105e6992c3bca70b99c2609d072fcd321328e2
TheWizardKing <sharmamohit53998@gmail.com>
2026-09-04 15:37:42 +0530

    Find all the pairs with given sum in the doubly linked list

--- LinkedList/FindGivenSumPairsInDoubleLL.cpp
+++ LinkedList/FindGivenSumPairsInDoubleLL.cpp
@@ -18,29 +18,30 @@ public:
 class Solution {
 public:
     vector<vector<int>> findPairsWithGivenSum(ListNode* head, int target) {
-        if(head == NULL) return (vector<vector<int>>){};
-        ListNode* i = head;
+        if(head == NULL) return (vector<vector<int>>){};  //if linked list is empty, then return empty vector
+
+        ListNode* i = head;    //using two pointers approach in linked lists
         ListNode* j = head;
 
-        while(j->next != NULL && j->next->val <= target){
+        while(j->next != NULL && j->next->val <= target){   //place the end/j pointer at the node having value <= target
             j = j->next;
         }
 
         vector<vector<int>> answer;
 
-        while(i != j && j != i->prev){
+        while(i != j && j != i->prev){     //now check if i+j == target, if yes then save the pair and shrink the pointers
             
-            if(i->val + j->val < target){
+            if(i->val + j->val < target){   //if pair sum < target, then push the start pointer forward (as we need to increase the sum)
                 i = i->next;
-            }else if(i->val + j->val > target){
+            }else if(i->val + j->val > target){    //if pair sum > target, then push the end pointer backwards (as we need to decrease the sum)
                 j = j->prev;
-            }else{
-                answer.push_back((vector<int>){i->val, j->val});
+            }else{    //if pair sum == target, save the pair and shrink both pointers
+                answer.push_back((vector<int>){i->val, j->val});   
 
                 i = i->next;
                 j = j->prev;
             }
         }
-        return answer;
+        return answer;   //return all the pair's vector as vector of vector
     }
 };
