0c7bf543a3474bd480cac28950d438df1834d560
TheWizardKing <sharmamohit53998@gmail.com>
2026-09-06 23:55:48 +0530

    Make Deep copy of the linked list having random pointers to random nodes in list

--- LinkedList/DeepCopyWithRandomPointerInLL.cpp
+++ LinkedList/DeepCopyWithRandomPointerInLL.cpp
@@ -19,25 +19,25 @@ class Solution {
 public:
     Node* copyRandomList(Node* head) {
 
-        unordered_map<Node*, Node*> mp;
+        unordered_map<Node*, Node*> mp;   //unordered_map to store mappings (instead of normal map because unordered_map has O(logn) time comp)
         Node* temp = head;
         Node* newHead = NULL;
 
-        while(temp != NULL){
+        while(temp != NULL){   //traversal of the original linked list and creating new nodes and simultaneously mapping original nodes to corresponding new nodes
             mp[temp] = new Node(temp->data);
-            if(newHead == NULL) newHead = mp[temp];
+            if(newHead == NULL) newHead = mp[temp];   //store first node's address as newHead
             temp = temp->next;
         }
-
-        temp = head;
+ 
+        temp = head;   //reset temp for another traversal to update new list's connections
 
         while(temp != NULL){
-            mp[temp]->next = mp[temp->next];
+            mp[temp]->next = mp[temp->next];   //update the new list node's connections using the map
             mp[temp]->random = mp[temp->random];
             temp = temp->next;
         }
 
-        return newHead;
+        return newHead;  //return the head of the new linked list
 
     }
 };
