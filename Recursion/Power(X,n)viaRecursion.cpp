fc21aa66b1d84443591103f70bf7b377a5cc3747
TheWizardKing <sharmamohit53998@gmail.com>
2026-09-24 00:08:00 +0530

    calculate the myPow(X,n) function's value

--- Recursion/Power(X,n)viaRecursion.cpp
+++ Recursion/Power(X,n)viaRecursion.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 double myPow(double x, int n) {
     long long N = n;
-    if(N<0){
+    if(N<0){     //handle the negative powers case by converting it to positive and taking reciprocal of x, rest everything is same
         N = -N;
         x = 1/x;
     }
-    if(N==0) return 1;
-    if(N%2 == 0){
+    if(N==0) return 1;     //base case
+    if(N%2 == 0){      //until the power is divisible by 2, we keep halving the power and multiplying the result with itself
         double half = myPow(x, N/2);
         return half*half;
     }
-    return x*myPow(x,N-1);
+    return x*myPow(x,N-1);   //if power is odd, we reduce the power by 1, and repeat the process of halving the exponent until base case
 }
 
 int main(){
